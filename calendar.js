21b8e7b1095bd7f6bee0dc78bce0df333b34ca9c
leo <leo7ocean@gmail.com>
2025-12-26 18:37:05 -0300

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,11 +1,23 @@
+const calendario = document.getElementById("calendario");
+const hoje = new Date();
+const ano = hoje.getFullYear();
+const mes = hoje.getMonth();
+
 fetch("reservas.json")
 .then(r => r.json())
-.then(dados => {
-  const div = document.getElementById("calendario");
-  dados.forEach(d => {
-    const p = document.createElement("p");
-    p.textContent = d + " - ALUGADA";
-    p.style.color = "red";
-    div.appendChild(p);
-  });
+.then(reservas => {
+  const diasNoMes = new Date(ano, mes + 1, 0).getDate();
+
+  for (let i = 1; i <= diasNoMes; i++) {
+    const data = `${ano}-${String(mes+1).padStart(2,"0")}-${String(i).padStart(2,"0")}`;
+    const div = document.createElement("div");
+    div.className = "day";
+    div.innerText = i;
+
+    if (reservas.includes(data)) {
+      div.classList.add("alugada");
+    }
+
+    calendario.appendChild(div);
+  }
 });
